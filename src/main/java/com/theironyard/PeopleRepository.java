84931e840952a67ecf82cb3ce58a26fa526f7ce6
Chris Tang <cwolf0913@gmail.com>
2017-04-17 08:45:46 -0400

    finished assignment

---
+++ src/main/java/com/theironyard/PeopleRepository.java
@@ -0,0 +1,76 @@
+package com.theironyard;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PeopleRepository {
+
+    @Autowired
+    JdbcTemplate template;
+
+    public List<Person> listPeople(String search){
+        return template.query("SELECT * FROM person WHERE " +
+                        "LOWER (firstname) LIKE (?) OR " +
+                        "LOWER (lastname) LIKE (?) " +
+                        "limit 100",
+                new Object[]{"%" + search + "%", "%" + search + "%"},
+                (rs, i) -> new Person(
+                        rs.getInt("personid"),
+                        rs.getString("title"),
+                        rs.getString("firstname"),
+                        rs.getString("middlename"),
+                        rs.getString("lastname"),
+                        rs.getString("suffix")
+                )
+        );
+    }
+
+    public Person getPersonId(Integer personId) {
+        return template.queryForObject("SELECT * FROM person WHERE " +
+                        "personid = ?",
+                new Object[]{personId},
+                (rs, i) -> new Person(
+                        rs.getInt("personid"),
+                        rs.getString("title"),
+                        rs.getString("firstname"),
+                        rs.getString("middlename"),
+                        rs.getString("lastname"),
+                        rs.getString("suffix")
+                )
+        );
+    }
+
+
+    public void savePerson(Person person) {
+        if (person.getPersonId() == null) {
+            template.update("INSERT INTO (title, firstname, middlename, lastname, suffix) " +
+                    "VALUES (?, ?, ?, ?, ?)",
+                    new Object[]{person.getTitle(),
+                    person.getFirstName(),
+                    person.getMiddleName(),
+                    person.getLastName(),
+                    person.getSuffix()}
+            );
+        } else {
+            template.update("UPDATE person SET " +
+                    "title = ?, " +
+                    "firstname = ?, " +
+                    "middlename = ?, " +
+                    "lastname = ?, " +
+                    "suffix = ? " +
+                    "WHERE personid = ?",
+                    new Object[]{person.getTitle(),
+                    person.getFirstName(),
+                    person.getMiddleName(),
+                    person.getLastName(),
+                    person.getSuffix(),
+                            person.getPersonId()}
+            );
+        }
+    }
+}
